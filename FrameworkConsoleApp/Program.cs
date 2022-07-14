5dc382f11b146c3463a605b1b0159b51612e3aee
Prince Azih <lajaylawrence@google.com>
2022-07-14 08:34:47 -0700

    edcew

---
+++ FrameworkConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrameworkConsoleApp
+{
+    class Program
+    {
+        private static readonly ManualResetEvent manualEvent = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            for(int index=0;index<5;index++)
+            {
+                Console.WriteLine("Docker image is ready");
+
+            }
+            manualEvent.WaitOne();
+        }
+    }
+}
